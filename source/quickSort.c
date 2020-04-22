9d0da5c6d45e826058aae97af19e5227c052e7a8
SANG-MIN PARK <sanggoe0509@gmail.com>
2020-04-23 03:15:50 +0900

    04 quick sort

--- source/quickSort.c
+++ source/quickSort.c
@@ -7,8 +7,47 @@ Quick 정렬의 시간 복잡도 : => O(N*logN)
 #include <stdio.h>
 
 int number = 10;
-int data[10] = 
+int data[10] = {4, 9, 2, 5, 7, 10, 1, 3, 6, 8};
+
+void quickSort(int *data, int start, int end) {
+	if (start >= end ) { // 원소가 1개인 경우
+		return;
+	}
+	
+	int key = start; // 키는 첫번째 원소
+	int i = start + 1;
+	int j = end;
+	int temp;
+	
+	while (i<=j) { // 엇갈릴 때 까지 반복 
+		while (data[i] <= data[key]) { // 키 값보다 큰 값을 만날 때 까지 
+			i++;
+		}
+		while (data[j] >= data[key] && j > start) { // 키 값보다 작은  값을 만날 때 까지 
+			j--;
+		}
+		if (i>j) { // 현재 엇갈린 상태면 키 값과 교체 
+			temp = data[j];
+			data[j] = data[key];
+			data[key] = temp;
+		} else {
+			temp = data[j];
+			data[j] = data[i];
+			data[i] = temp;
+		}
+		
+		// 함수의 재귀적 호출을 통해 정렬을 반복 수행 해준다. 
+		quickSort(data, start, j-1);
+		quickSort(data, j+1, end);
+	}
+}
 
 int main(void) {
+	int i;
 	
+	quickSort(data, 0, number-1);
+	
+	for (i=0; i<number; i++) {
+		printf("%d ", data[i]);
+	}
 }
