0ae4f0167a4e5bf9724058e3e40b9ec6d04c340d
SANG-MIN PARK <sanggoe0509@gmail.com>
2020-04-23 01:52:38 +0900

    04 quick sort

---
+++ source/quickSort.c
@@ -0,0 +1,14 @@
+/* Quick Sort Algorithm
+특정한 값을 기준으로 큰 숫자와 작은 숫자를 서로 교환한 뒤에 배열을 반으로 나누는 방식 
+Quick 정렬의 시간 복잡도 : => O(N*logN)
+* 앞선 정렬 알고리즘 중에 효율이 가장 뛰어난 알고리즘 
+*/
+
+#include <stdio.h>
+
+int number = 10;
+int data[10] = 
+
+int main(void) {
+	
+}
